df45c67f74a2d710816f15ec8f4172e0967ee64a
Toheed <toheedjamaal9@gmail.com>
2022-07-16 03:54:50 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,22 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        iconTheme: IconThemeData(size: 40, color: Colors.black),
+        actions: [
+          Builder(
+            builder: (context) => IconButton(
+              icon: const Icon(Icons.menu_open_rounded),
+              onPressed: () => Scaffold.of(context).openEndDrawer(),
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            ),
+          ),
+        ],
+        iconTheme: const IconThemeData(size: 40, color: Colors.black),
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: const Center(
-        child: Text('home!'),
-      ),
+      body: Container(),
       endDrawer: Drawer(
+        //elevation: 10,
         width: 210,
         // Add a ListView to the drawer. This ensures the user can scroll
         // through the options in the drawer if there isn't enough vertical
@@ -38,40 +46,76 @@ class MyHomePage extends StatelessWidget {
           // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: [
-            const UserAccountsDrawerHeader(
-              // <-- SEE HERE
-              decoration: BoxDecoration(color: Colors.black),
-              accountName: Text(
-                "Account Name",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+            Wrap(
+              children: [
+                UserAccountsDrawerHeader(
+                  // <-- SEE HERE
+                  decoration: const BoxDecoration(color: Colors.black26),
+
+                  accountName: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "Account Name",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  accountEmail: null,
+                  currentAccountPicture: const FlutterLogo(),
                 ),
-              ),
-              accountEmail: Text(
-                "accountmail@mail.com",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+                ListTile(
+                  leading: const Icon(Icons.account_box),
+                  title: const Text('Account'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
                 ),
-              ),
-              currentAccountPicture: FlutterLogo(),
-            ),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
+                const Divider(
+                  color: Colors.black54,
+                ),
+                ListTile(
+                  leading: const Icon(Icons.settings),
+                  title: const Text('Setting'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                const Divider(
+                  color: Colors.black54,
+                ),
+                ListTile(
+                  leading: const Icon(Icons.help),
+                  title: const Text('help'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+                const Divider(
+                  color: Colors.black54,
+                ),
+                ListTile(
+                  leading: const Icon(Icons.logout),
+                  title: const Text('logout'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
             ),
           ],
         ),
@@ -79,3 +123,4 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
+
